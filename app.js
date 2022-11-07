30f9b670c3a0c2fb969ad3cc2170a8e45b03343b
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-07 16:24:48 -0500

    added a try/catch block to catch any unexpected errors. If an error occurs there is a default background image applied to the page. Also added an element to render the author name

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const authorName = document.querySelector(".author-name");
+
 async function getBackgroundImg() {
-  const res = await fetch(
-    "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
-  );
-  const data = await res.json();
-  document.querySelector(
-    "body"
-  ).style.backgroundImage = `url(${data.urls.raw})`;
-  console.log(data);
+  try {
+    const res = await fetch(
+      "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+    );
+    const data = await res.json();
+    document.querySelector(
+      "body"
+    ).style.backgroundImage = `url(${data.urls.raw})`;
+    console.log(data);
+    authorName.textContent = data.user.name;
+  } catch (err) {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+  }
 }
 
 getBackgroundImg();
