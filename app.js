16de8c014141152b37501b89973d3714c1ee2fbc
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-08 12:24:46 -0500

    quick change to the photography credit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ async function getBackgroundImg() {
       "body"
     ).style.backgroundImage = `url(${data.urls.raw})`;
     // console.log(data);
-    authorName.textContent = data.user.name;
+    authorName.textContent = `Photographer: ${data.user.name}`;
   } catch (err) {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   }
