e86fcea4a6276cec4206ddad9c872bdf8bcfc744
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-11-09 12:02:18 -0500

    built out the geolocating function to provide the users longitute and latitude

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/* 
+  Author: Travis Brown
+*/
+
+// Provides random background image for the dashboard, credits photographer.
 async function getBackgroundImg() {
   const authorName = document.querySelector(".author-name");
   try {
@@ -8,13 +13,13 @@ async function getBackgroundImg() {
     document.querySelector(
       "body"
     ).style.backgroundImage = `url(${data.urls.raw})`;
-    // console.log(data);
     authorName.textContent = `Photographer: ${data.user.name}`;
   } catch (err) {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   }
 }
 
+// gets latest Bitcoin market data and renders to dashboard.
 async function getPrice() {
   try {
     const cryptoContainer = document.querySelector(".crypto-container");
@@ -43,20 +48,32 @@ async function getPrice() {
   }
 }
 
+// provides clock functionality.
 function time() {
   const time = new Date().toLocaleTimeString([], { timeStyle: "short" });
   document.querySelector(".time").textContent = time;
 }
 setInterval(time, 1000);
 
-// const options = {
-//   enableHighAccuracy: true,
-//   timeout: 10000,
-// };
-
+/* gets user location to provide weather api with lat and lon
+   then renders that data to the weather section.
+*/
 navigator.geolocation.getCurrentPosition((position) => {
-  console.log(position);
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Data currently unavailable");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.error(error));
 });
 
+// function calls.
 getBackgroundImg();
 getPrice();
